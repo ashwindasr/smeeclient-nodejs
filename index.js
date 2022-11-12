db4a1f8f1ca8b49a4fd71965086d42ab44e5db18
Ashwin Das <ashwindasr@gmail.com>
2022-11-11 20:17:46 -0500

    fix2

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const SmeeClient = require('smee-client')
 
-function getValue(data) {
-  const routes = data.routes;
-
-  for(const key in routes) {
-    const value = routes[key];
-    const smeeSource = value["SMEESOURCE"];
-    const httpTraget = value["HTTPTARGET"];
-
-
-    const smee = new SmeeClient({
-      source: smeeSource,
-      target: httpTraget,
-      logger: console
-    });
-
-    smee.start();
-  }
-}
-
-const myHeaders = new Headers({
-  'Authorization': 'token ' + process.env.GITHUBTOKEN,
-});
-
 fetch(process.env.JSONPATH, {
-  headers: myHeaders,
-  method: 'GET'
+    headers: {
+        'Authorization': 'token ' + process.env.GITHUBTOKEN,
+    },
+    method: 'GET'
 }).then(response => response.json())
     .then(data => getValue(data));
 
+function getValue(data) {
+    const routes = data.routes;
+
+    for (const key in routes) {
+        const value = routes[key];
+        const smeeSource = value["SMEESOURCE"];
+        const httpTraget = value["HTTPTARGET"];
+
+
+        const smee = new SmeeClient({
+            source: smeeSource,
+            target: httpTraget,
+            logger: console
+        });
+
+        smee.start();
+    }
+}
